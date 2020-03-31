fa391babbb430fcc030d6dcaaf9fe176901a8209
goyaltanya <53858535+goyaltanya@users.noreply.github.com>
2020-03-31 21:39:13 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,23 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 7;
+        while(i>0){
+            tree.add(scanner.nextInt());
+            i--;
+        }
+
+        tree.printLeftChild();
+
+        System.out.println(MyBinarySearchTree.noLeftCounter);
+    }
 }
